8981ed0508f2e294b5d0f1d8c787041baa2c1cfb
ThomasCl <56159161+ThomasCl@users.noreply.github.com>
2023-12-05 18:20:19 +0100

    user backend

---
+++ backend/src/main/java/backend/model/User.java
@@ -0,0 +1,75 @@
+// User.java
+package backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users", schema = "public")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+    private String first_name;
+    private String last_name;
+    private boolean has_voted;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFirstName() {
+        return first_name;
+    }
+
+    public String getLastName() {
+        return last_name;
+    }
+
+    public boolean getHasVoted() {
+        return has_voted;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setFirstName(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public void setLastName(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public void setHasVoted(boolean has_voted) {
+        this.has_voted = has_voted;
+    }
+
+}
