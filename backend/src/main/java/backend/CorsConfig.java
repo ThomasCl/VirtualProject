c0295f0ebdd0774e38a51d7ae246a1e62c846110
ThomasCl <56159161+ThomasCl@users.noreply.github.com>
2023-12-08 23:10:43 +0100

    cors

---
+++ backend/src/main/java/backend/CorsConfig.java
@@ -0,0 +1,24 @@
+package backend;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://192.168.0.216:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
+    }
+}
