3fb808fa83135b7f60c8062009a03d89430d6317
ThomasCl <56159161+ThomasCl@users.noreply.github.com>
2024-01-15 17:24:59 +0100

    blue green

---
+++ determine_color.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Logic to determine the color (e.g., alternating between blue and green)
+CURRENT_COLOR=$(curl -s https://raw.githubusercontent.com/ThomasCl/VirtualProject/main/color.txt)
+if [ "$CURRENT_COLOR" == "blue" ]; then
+  echo "green" > color.txt
+else
+  echo "blue" > color.txt
+fi
+echo $CURRENT_COLOR
