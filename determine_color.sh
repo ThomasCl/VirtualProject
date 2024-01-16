8ce41ddc09221bbba438797bed13cd0b043ad181
ThomasCl <56159161+ThomasCl@users.noreply.github.com>
2024-01-16 18:02:19 +0100

    made it so blue/green actually changes

--- determine_color.sh
+++ determine_color.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Logic to determine the color (e.g., alternating between blue and green)
-CURRENT_COLOR=$(curl -s https://raw.githubusercontent.com/ThomasCl/VirtualProject/master/color.txt)
-if [ "$CURRENT_COLOR" == "blue" ]; then
+CURRENT_COLOR=$(curl -s https://raw.githubusercontent.com/ThomasCl/VirtualProject/master/color.txt | tr -d '"')
+if [ "$CURRENT_COLOR" = "blue" ]; then
   echo "green" > color.txt
 else
   echo "blue" > color.txt
